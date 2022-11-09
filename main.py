8faf61e7666bbe9442cef5a5f199fc959659527d
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-11-09 17:58:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+# from dynamic_functions import *
+
+from function_interactions import mix_sticks
+
+sticks = ["-", "--", "---", "----","-----"]
+my_mix = mix_sticks(sticks)
+
+# try1 = try_your_luck()
+# print(try1)
+
+# print(verify_my_try(my_mix, try1))
+
